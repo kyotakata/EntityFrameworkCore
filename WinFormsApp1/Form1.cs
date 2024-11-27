be411d5b7f2f5ffa93a902e4633787f925acc218
Kyo Takata <kyotakata1@gmail.com>
2024-11-28 01:39:41 +0900

    5.B04_データベースとの接続

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,11 +8,28 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var builder = new SqlConnection
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-O4DS0L0";
+            builder.InitialCatalog = "AndersonA";
+            builder.IntegratedSecurity = true;// Windows認証のためtrue.サーバー認証にしている場合はfalseでユーザー名とパスワードを入れる
+            builder.TrustServerCertificate = true;
+
+            string sql = "select getdate()";// SQL文字列
+
+            DataTable dt = new DataTable();
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            using (var adapter = new SqlDataAdapter(sql,connection))
+            {
+                connection.Open();
+                adapter.Fill(dt);//Fillするとsqlが実行される。データテーブル(dt)に結果が入る
+            }
+
+            dataGridView1.DataSource = dt;
         }
     }
 }
